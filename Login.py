e8b5ae8143b4cac39e257306edc69810e32d08e0
narges <nargesvahedy0310.gmail.com>
2022-07-30 11:10:36 -0700

    Login_instagram

---
+++ Login.py
@@ -0,0 +1,37 @@
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+class bot:
+    def __init__(self, username, password):
+        self.driver = webdriver.Chrome("C:/Users/Narges/Desktop/test1/test1/chromedriver.exe")
+        self.username = username
+        self.password = password
+    def Login(self):
+        driver=self.driver
+        driver.get('https://www.instagram.com/accounts/login/?next=/accounts/')
+        driver.maximize_window()
+        time.sleep(2)
+        for u in username:
+            r=random.uniform(0.1,1.5)
+            time.sleep(r)
+            box_username=driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(u)
+        time.sleep(time_rondom)
+        for p in password:
+            r=random.uniform(0.1,1.5)
+            time.sleep(r)
+            box_username=driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(p)
+        time.sleep(time_rondom)
+        btn_login=driver.find_element(By.XPATH,'//*[@id="loginForm"]/div/div[3]/button').click()
+        time.sleep(7)
+        save_info=driver.find_element(By.XPATH,'//*[@id="react-root"]/section/main/div/div/div/section/div/button').click()
+        time.sleep(5)
+username='username'
+password='password'
+time_rondom=random.randint(2,5)
+test=bot(username, password)
+test.Login()
+test.Like()
+
